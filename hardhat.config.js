8243dad56fcb8930cbc4ccde13ecc522ce05e79f
Sujon <160887695+sujon12210@users.noreply.github.com>
2026-01-22 20:45:48 +0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  paths: {
+    sources: "./", // Root directory for contracts
+    tests: "./",   // Root directory for tests
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
